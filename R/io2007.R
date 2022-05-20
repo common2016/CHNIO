1edc5b05a5ee150ebc0aa0fabdc2ea69435a114b
ChenPu <yangnay@192.168.3.45>
2022-05-20 17:54:45 +0800

    io2007

---
+++ R/io2007.R
@@ -0,0 +1,6 @@
+#' Input Output Tables at 2007
+#'
+#'
+#' A list that includes 31 provinces' IO tables at 2007.
+#'
+'io2007'
---
+++ R/io2012.R
@@ -0,0 +1,6 @@
+#' Input Output Tables at 2012
+#'
+#'
+#' A list that includes 31 provinces' IO tables at 2012.
+#'
+'io2012'
