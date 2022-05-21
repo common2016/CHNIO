a67d89d96a6e6aac2a5019b44a7f21e10441b7cd
ChenPu <yangnay@192.168.3.45>
2022-05-21 10:41:22 +0800

    secnames

---
+++ R/secnames.R
@@ -0,0 +1,16 @@
+#' Sector Names of Input Output Tables
+#'
+#'
+#' A dataset that includes sector names, sector code and years.
+#'
+#' @format A data frame:
+#' \describe{
+#' \item{nm}{sector names}
+#' \item{code}{sector codes}
+#' \item{year02}{If the sector exists at 2002, it's marked by 2002}
+#' \item{year07}{If the sector exists at 2007, it's marked by 2007}
+#' \item{year12}{If the sector exists at 2012, it's marked by 2012}
+#' \item{year17}{If the sector exists at 2017, it's marked by 2017}
+#' }
+#'
+'secnames'
--- data-raw/io.R
+++ data-raw/io.R
@@ -4,6 +4,41 @@ library(pacman)
 p_load(stringr, devtools)
 load_all()
 
+# 代码和名称
+flexcel <- paste('data-raw/中国31省区市42部门投入产出表1997-2017皮皮侠/','2002年各省份投入产出表', sep = '') %>% dir()
+fl <- flexcel[!str_detect(flexcel, '^~')] %>%
+  paste('data-raw/中国31省区市42部门投入产出表1997-2017皮皮侠','2002年各省份投入产出表',., sep = '/')
+
+secnames <- xlsx::read.xlsx(fl[2],1,rowIndex = c(3,6), colIndex = 4:57, header = FALSE) %>%
+  t() %>% as.data.frame()
+names(secnames) <- c('nm','code')
+secnames$nm[secnames$code %in% 'FU101'] <- '农村居民消费'
+secnames$nm[secnames$code %in% 'FU102'] <- '城镇居民消费'
+secnames$nm[secnames$code %in% 'THC'] <- '居民消费小计'
+secnames$nm[secnames$code %in% 'FU103'] <- '政府消费'
+secnames$nm[secnames$code %in% 'TC'] <- '消费小计'
+secnames$nm[secnames$code %in% 'FU201'] <- '固定资本形成总额'
+secnames$nm[secnames$code %in% 'FU202'] <- '存货增加'
+secnames$nm[secnames$code %in% 'GCF'] <- '资本形成总额合计'
+
+ans <- xlsx::read.xlsx(fl[2],1,rowIndex = c(49:55), colIndex = 2:3, header = FALSE)
+names(ans) <- c('nm','code')
+ans$nm[ans$code %in% 'TI'] <- '总投入'
+secnames <- rbind(secnames, ans)
+secnames$year02 <- 2002
+secnames$year07 <- 2007
+secnames$year12 <- 2012
+secnames$year17 <- 2017
+# 2007
+secnames <- data.frame(nm = c('流出','流入','其他'),code = c('FU302','FU304','ERR'),year02 = NA,
+                       year07 = 2007, year12 = c(NA,NA,2012), year17 = c(NA,NA,2017)) %>% rbind(secnames, .)
+# 2012,2017
+secnames <- data.frame(nm = c('出口','国内省外流出','进口','国内省外流入'),code = c('EX','OF','IM','IF'),year02 = NA,
+                       year07 = NA, year12 = 2012, year17 = 2017) %>% rbind(secnames, .)
+secnames$nm <- str_remove_all(secnames$nm,'\n')
+
+# usethis::use_data(secnames, overwrite = TRUE)
+
 # 2002
 io2002 <- getio('2002年各省份投入产出表')
 
